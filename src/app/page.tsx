93ba1de8132582b008901b3c74eeb87c62a265b2
flangham <finnian.langham@gmail.com>
2025-06-17 12:10:56 +0200

    Configure typestyles + load fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
     <main>
+      <h1 className="t-d1">Fairhaven Home</h1>
+      <h1 className="t-h1">Fairhaven Home</h1>
       <h1 className="t-h2">Fairhaven Home</h1>
+      <h1 className="t-h3">Fairhaven Home</h1>
+      <h1 className="t-h4">Fairhaven Home</h1>
+      <h1 className="t-h5">Fairhaven Home</h1>
+      <h1 className="t-b1">Fairhaven Home</h1>
+      <h1 className="t-b2">Fairhaven Home</h1>
+      <h1 className="t-b3">Fairhaven Home</h1>
+      <h1 className="t-button">Fairhaven Home</h1>
+      <h1 className="t-label">Fairhaven Home</h1>
+      <h1 className="t-tag">Fairhaven Home</h1>
+      <h1 className="t-caption">Fairhaven Home</h1>
     </main>
   );
 }
