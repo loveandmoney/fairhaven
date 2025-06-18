98cccf8594310ea6c12adc7662e5d301249bb8c9
flangham <finnian.langham@gmail.com>
2025-06-18 13:18:06 +0200

    remove checks during build

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,12 @@ const nextConfig: NextConfig = {
         pathname: '/**'
       }
     ]
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true
   }
 };
 
